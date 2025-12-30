5b94043fd9bdebc6565ec66e8491b621ce282cb6
ultra-code <mangobear1217@gmail.com>
2025-12-30 12:50:48 +0900

    251230 - Updated yutaek.cpp file
    출력문 새로 하나 만듦

--- Comprehention_Test_251230/TestYutaek.cpp
+++ Comprehention_Test_251230/TestYutaek.cpp
@@ -3,5 +3,10 @@
 using namespace std;
 
 class TestYutaek {
-	// 문자열 출력하는 클래스 아무거나 작성
+public: // 1. 외부에서 이 함수를 호출할 수 있도록 공개 설정
+
+    // 2. 문자열을 출력하는 'printMessage' 함수 정의
+    void printMessage() {
+        cout << "안녕하세요! TestYutaek 클래스에서 출력하는 메시지입니다." << endl;
+    }
 };
