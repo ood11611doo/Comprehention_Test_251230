3a068beff827236903c547e3e2e8074710174eca
kimsanghoon121 <na773014@gmail.com>
2025-12-30 12:49:22 +0900

    251230 Update sanghoon
    수정한 부분
    이거이거 했음
    저거저거 했음

--- Comprehention_Test_251230/TestSanghoon.cpp
+++ Comprehention_Test_251230/TestSanghoon.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
-class TestSanghoon {
-	// 문자열 출력하는 클래스 아무거나 작성
+class TestSanghoon 
+{
+public:
+	void print()
+	{
+		cout << "6조 김상훈" << endl;
+	}
+
+	// 문자열 출력하는 함수 아무거나 작성
 };
