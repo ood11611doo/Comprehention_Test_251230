23128386ac1a3adfb823ebbe53521b11c3b99e5a
Yoongisang <sawsd@naver.com>
2025-12-30 12:39:12 +0900

    Update TestKisang.cpp

--- Comprehention_Test_251230/TestKisang.cpp
+++ Comprehention_Test_251230/TestKisang.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 
 class TestKisang {
-	// 문자열 출력하는 클래스 아무거나 작성
+public:
+	// 문자열 출력하는 함수 아무거나 작성
+	void printName() {
+		cout << "Gisang" << '\n';
+	}
 };
